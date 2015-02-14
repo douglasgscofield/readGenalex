bc8bbec60b38ff4531af2d9c186f22c1cda7ffee
Douglas G. Scofield <douglasgscofield@gmail.com>
2015-02-14 08:26:17 +0100

    Start incorporating checkNullAlleles

--- R/checkNullAlleles.R
+++ R/checkNullAlleles.R
@@ -1,37 +1,34 @@
-# Copyright (c) 2011 Douglas G. Scofield, Umeå Plant Science Centre, Umeå, Sweden
-#
-# douglas.scofield@plantphys.umu.se
-# douglasgscofield@gmail.com
-#
-# Compare genotypes against a set of reference genotypes and look for genotypes
-# that differ in a way consistent with the occurrence of a null allele, that is,
-# heterozygous in the reference and homozygous matching one of the alleles in
-# the heterozygous reference.
-#
-# The reference and compare genotypes are in GenAlEx format, and are either read
-# in within checkNullAlleles() or are presented as dataframes read by
-# readGenalex().  readGenalex() and its associated functions must be available,
-# and can be found at http://sites.google.com/site/douglasgscofield
-#
-# CHANGELOG
-#
-# 0.2: A few corrections
-# 0.1: First public release
-#
-.checkNullAllelesVersion <- "0.2"
-
-# Function for checking for null alleles in comparegt, when compared against
-# refgt.  refgt and comparegt may be filenames (files must be in GenAlEx format)
-# or they may be a GenAlEx dataframe loaded earlier using the read.genalex()
-# function (included in a separate file.)
-#
-# Set quiet=TRUE if you don't want checkNullAlleles to print informational 
-# messages in addition to the null allele reports.
-#
-# Use as you see fit.  No warranty regarding this code is implied nor should be
-# assumed.  Send bug reports etc. to one of the above email addresses.
-
-checkNullAlleles <- function(refgt, comparegt, quiet=FALSE)
+#' Check for potential null (non-amplifying) alleles
+#'
+#' 
+#' Compare genotypes against a set of reference genotypes and look for
+#' genotypes that differ in a way consistent with the occurrence of a null
+#' allele, that is, heterozygous in the reference and homozygous matching one
+#' of the alleles in the heterozygous reference.
+#' 
+#' The reference and compare genotypes are in GenAlEx format, and are either
+#' names of files to be read as GenAlEx files, or are class \code{'genalex'}
+#' data frames.
+#' 
+#' @param quiet  \code{TRUE} or \code{FALSE}, if \code{TRUE} then informational
+#'               messages are printed during the check for null alleles
+#' 
+#' @return Nothing.  Informational messages are printed during its execution.
+#'         Perhaps this should be changed so that it returns a list of
+#'         coordinates within the genotypes that represent potential null
+#'         alleles?
+#' 
+#' @author Douglas G. Scofield
+#' 
+#' @examples
+#' 
+#' # load reference genotypes
+#' # load comparison genotypes
+#' # run comparison
+#' 
+#' @export checkNullAlleles
+#' 
+checkNullAlleles <- function(refgt, comparegt, quiet = FALSE)
 {
     if (is.character(refgt))
         refgt <- readGenalex(refgt)
